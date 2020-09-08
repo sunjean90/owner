920432f09db61565361ce67ac9f732903a120b10
sunjian <sunjian@joyoudata.com>
2020-09-08 16:05:58 +0800

    no message

---
+++ src/main/java/com/helpful/util/Easy_Ing/App.java
@@ -0,0 +1,13 @@
+package com.helpful.util.Easy_Ing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
